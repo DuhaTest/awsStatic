47eb0d7dd009e619bb0121d4f056594b3f916565
dhamdan <dhamdan@atypon.com>
2022-08-16 11:54:29 +0300

    fix issuse

--- src/Page/Page.stories.jsx
+++ src/Page/Page.stories.jsx
@@ -9,6 +9,7 @@ export default {
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
+    chromatic: { viewports: [360, 1500] },
   },
 };
 
