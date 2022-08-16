89f4ca092af4edd466e0b25263bf834fbd89cf5d
dhamdan <dhamdan@atypon.com>
2022-08-16 10:24:27 +0300

    add storybook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Button/Button";
+import {Page} from "./Page/Page";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Page></Page>
     </div>
   );
 }
